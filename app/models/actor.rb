034727fbc0280a1ea490818b1489294be93021c5
max anselmo <itmanselmo@gmail.com>
2021-11-08 22:22:13 -0500

    lab completed

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,18 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows
+
+    def shows
+        self.characters.map { |c| c.show }
+    end
+
+    def full_name
+        self.first_name + " " + self.last_name
+    end
+
+    def list_roles
+        self.characters.map do |c|
+            "#{c.name} - #{c.show.name}"
+        end
+    end
 end
